c7003d0374f1c2abb695ed9d589296a7ad9a721d
Gillett Hernandez <gillett.hernandez@gmail.com>
2021-01-25 06:46:44 -0800

    add calculated pressure and temperature readouts

--- src/bin/particles.rs
+++ src/bin/particles.rs
@@ -1,6 +1,8 @@
 #![feature(slice_fill)]
 extern crate minifb;
 
+use std::f32::consts::SQRT_2;
+
 use lib::*;
 
 use minifb::{Key, KeyRepeat, MouseButton, MouseMode, Scale, Window, WindowOptions};
@@ -49,6 +51,11 @@ impl Particle {
         self.vy += self.ay * dt;
         self.x += self.vx * dt;
         self.y += self.vy * dt;
+    }
+    pub fn energy(&self) -> f32 {
+        let speed = self.vx.hypot(self.vy);
+
+        0.5 * self.mass * speed.powi(2)
     }
 }
 
@@ -156,7 +163,7 @@ fn main() {
     particles.push(particle);
     for i in 0..num_particles - 1 {
         let phi = random::<f32>() * std::f32::consts::TAU;
-        let mag = random::<f32>() * 0.3 + 0.2;
+        let mag = random::<f32>() * 0.0 + 0.4;
         let r = random::<f32>() * 0.003 + 0.003;
         let particle = loop {
             let x = random::<f32>() * (1.0 - 2.0 * r) + r;
@@ -185,10 +192,12 @@ fn main() {
         particles.push(particle);
     }
 
+    let num_small_particles = particles.len() as f32 - 1.0;
+
     // let mut swap = particles.clone();
 
-    // for i in 0..num_particles {
-    //     for j in 0..num_particles {
+    // for i in 0..particles.len() {
+    //     for j in 0..particles.len() {
     //         if j <= i {
     //             break;
     //         }
@@ -202,8 +211,9 @@ fn main() {
 
     let mut events: Vec<(f32, f32, usize, Option<usize>)> = Vec::new();
 
-    let mut new_events: Vec<(f32, f32, usize, Option<usize>)> = (0..num_particles)
-        .map(move |i| (0..num_particles).map(move |j| (i, j)))
+    let length = particles.len();
+    let mut new_events: Vec<(f32, f32, usize, Option<usize>)> = (0..length)
+        .map(move |i| (0..length).map(move |j| (i, j)))
         .flatten()
         .filter_map(|(i, j)| {
             if i >= j {
@@ -216,7 +226,7 @@ fn main() {
             particle_particle_check(t, i, j, particle, other)
         })
         .collect();
-    for idx in 0..num_particles {
+    for idx in 0..particles.len() {
         // horizontal wall (vy) first
         let particle = particles[idx];
         let event = particle_wall_check(t, idx, &particle);
@@ -226,6 +236,9 @@ fn main() {
     }
     events.extend(new_events.drain(..));
     events.push((-1.0, t + frame_dt, 0, None));
+    let mut mean_free_path = 0.0;
+    let mut mean_pressure = 0.0;
+    let moving_average_constant = 0.999;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         film.buffer.fill(0u32);
@@ -266,25 +279,56 @@ fn main() {
         t = collision_time;
         if event_time < 0.0 {
             events.push((-1.0, t + frame_dt, 0, None));
-            for particle in particles.iter() {
+
+            let mut sum_energy = 0.0;
+            let mut sum_speed = 0.0;
+            let mut sum_sqr_speed = 0.0;
+            let (mut min_speed, mut max_speed, mut min_energy, mut max_energy) = (
+                std::f32::INFINITY,
+                std::f32::NEG_INFINITY,
+                std::f32::INFINITY,
+                std::f32::NEG_INFINITY,
+            );
+
+            let calculated_mean_free_path =
+                (4.0 * (0.003 + 0.003 * 0.5) * num_small_particles * SQRT_2).recip();
+            for particle in particles.iter_mut() {
                 let (px, py) = (
                     (particle.x * WINDOW_WIDTH as f32) as usize,
                     (particle.y * WINDOW_HEIGHT as f32) as usize,
                 );
                 attempt_write(&mut film, px, py, 255u32 + 255u32 << 8);
-                let PIXEL_DIAMETER = 1.0 / film.width as f32;
 
                 let mut r = 1;
                 let pixel_radius = loop {
-                    if r as f32 * PIXEL_DIAMETER > particle.radius {
+                    if r as f32 / film.width as f32 > particle.radius {
                         break r;
                     }
                     r += 1;
                 };
 
-                let e = 0.5 * particle.mass * particle.vx.hypot(particle.vy).powi(2);
+                let v = particle.vx.hypot(particle.vy);
+                // calculated_mean_free_path
+                if window.is_key_pressed(Key::LeftBracket, KeyRepeat::Yes) {
+                    particle.vx *= 0.9;
+                    particle.vy *= 0.9;
+                }
+                if window.is_key_pressed(Key::RightBracket, KeyRepeat::Yes) {
+                    particle.vx *= 1.1;
+                    particle.vy *= 1.1;
+                }
+                let e = particle.energy();
+                if particle.mass < 1.0 {
+                    sum_speed += v;
+                    sum_sqr_speed += v.powi(2);
+                    sum_energy += e;
+                }
+                min_speed = min_speed.min(v);
+                max_speed = max_speed.max(v);
+                min_energy = min_energy.min(v);
+                max_energy = max_energy.max(v);
                 let c = triple_to_u32(hsv_to_rgb(
-                    ((360.0 * (1.0 - (-e).exp())) as usize + 100) % 360,
+                    ((360.0 * (1.0 - (-e * 10.0).exp())) as usize + 100) % 360,
                     1.0,
                     1.0,
                 ));
@@ -292,6 +336,43 @@ fn main() {
 
                 attempt_write(&mut film, px, py, c);
             }
+
+            let temperature1 = mean_pressure / 4.0 * 1.0 / num_small_particles;
+            let temperature2 =
+                mean_free_path * 4.0 * (0.003 + 0.003 * 0.5) * SQRT_2 * mean_pressure / 4.0;
+
+            // PV = NkT
+            // k = PV / NT
+            let k = mean_pressure / 4.0 * 1.0
+                / num_small_particles
+                / ((temperature1 + temperature2) / 2.0);
+            let estimated_particles = mean_pressure / 4.0 * 1.0 / temperature2 / k;
+            let calculated_pressure = sum_sqr_speed / 2.0 / 4.0;
+            println!(
+                "av:{:.5}, ae:{:.5}, mfp:{:.5}, mp:{:.5}, t:{:.5}, N:{:.5}, k:{:.5} --- c:{:.5}, c2: {:.5}",
+                sum_speed / num_small_particles,
+                sum_energy / num_small_particles,
+                mean_free_path,
+                mean_pressure / 4.0,
+                temperature2,
+                estimated_particles,
+                k,
+                mean_pressure / 4.0 / (sum_energy / num_small_particles) / 144.0,
+                calculated_pressure
+            );
+
+            // print!(
+            //     "avg, min, max speed: {:?}, {:?}, {:?}, ",
+            //     sum_speed / particles.len() as f32,
+            //     min_speed,
+            //     max_speed
+            // );
+            // println!(
+            //     "avg, min, max energy: {:?}, {:?}, {:?}",
+            //     sum_energy / particles.len() as f32,
+            //     min_energy,
+            //     max_energy
+            // );
             // std::mem::swap(&mut particles, &mut swap);
             window
                 .update_with_buffer(&film.buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -315,13 +396,30 @@ fn main() {
                     // println!("performing actual collision. impulse is {} = 2.0 * {} * {} * {} / ({} * ({} + {}))", j, other.mass, particle.mass, dvdr, sigma, other.mass, particle.mass);
                     new_particle.vx += jxy.0 / particle.mass;
                     new_particle.vy += jxy.1 / particle.mass;
-                    let prev_other_v = other.vx.hypot(other.vy);
+
+                    let old_other_e = other.energy();
                     other.vx -= jxy.0 / other.mass;
                     other.vy -= jxy.1 / other.mass;
                     let new_particle_v = new_particle.vx.hypot(new_particle.vy);
-                    let new_particle_e = 0.5 * particle.mass * new_particle_v.powi(2);
+                    let old_particle_e = particle.energy();
+                    let new_particle_e = new_particle.energy();
                     let other_v = other.vx.hypot(other.vy);
-                    let other_e = 0.5 * other.mass * other_v.powi(2);
+                    let new_other_e = other.energy();
+                    let old_sum_energy = old_particle_e + old_other_e;
+                    let new_sum_energy = new_particle_e + new_other_e;
+
+                    // energy gain multiplier. scale velocity down by root of multiplier to preserve kinetic energy
+                    let mult = (new_sum_energy / old_sum_energy).sqrt();
+                    other.vx /= mult;
+                    other.vy /= mult;
+                    new_particle.vx /= mult;
+                    new_particle.vy /= mult;
+
+                    let new_sum_energy = new_particle.energy() + other.energy();
+
+                    if (new_sum_energy - old_sum_energy).abs() > 0.0001 {
+                        println!("conservation of kinetic energy violated, old: ({} + {} = {}), new: ({} + {} = {})", old_particle_e, old_other_e, old_other_e + old_particle_e, new_particle_e, new_other_e, new_particle_e + new_other_e);
+                    }
                     if new_particle_v > 3.0 {
                         println!("warning, very fast moving particle");
                         // panic!();
@@ -334,21 +432,43 @@ fn main() {
                         // other.vx /= other_v;
                         // other.vy /= other_v;
                     }
+                    let this_free_path = (new_particle.vx * (t - new_particle.time))
+                        .hypot(new_particle.vy * (t - new_particle.time));
+                    let that_free_path =
+                        (new_particle.vx * (t - other.time)).hypot(other.vy * (t - other.time));
+                    if particle.mass < 1.0 {
+                        mean_free_path = mean_free_path * moving_average_constant
+                            + (1.0 - moving_average_constant) * this_free_path;
+                    }
+                    if other.mass < 1.0 {
+                        mean_free_path = mean_free_path * moving_average_constant
+                            + (1.0 - moving_average_constant) * that_free_path;
+                    }
                     new_particle.time = t; // + std::f32::EPSILON;
                     other.time = t; // + std::f32::EPSILON;
                 }
                 None => {
                     new_particle.time = t;
+                    // force = mass * acceleration
+                    // pressure is force divided by area. since "area" in this simulation is 2d, it would be force divided by the "surface" of the container, or the perimeter of the container.
+                    // acceleration or force and be calculated as an elastic collision.
+                    // acceleration is such that the particles' momentum in the X direction changes from from -m * vx to m * vx. thus the momentum changes by 2 * m * vx in 1 frame. thus the acceleration is 2 * m * vx / dt. we will assume and scale such that dt = 1, however.
                     if particle.x - particle.radius < 0.001 {
                         // near left wall (x = 0)
                         // println!(
                         //     "bouncing horizontal, {}, {}, {}",
                         //     particle.x, particle.y, particle.radius
                         // );
-                        new_particle.vx *= -1.3;
+                        let force = 2.0 * particle.mass * new_particle.vx.abs() / frame_dt;
+                        mean_pressure = moving_average_constant * mean_pressure
+                            + (1.0 - moving_average_constant) * force;
+                        new_particle.vx *= -1.0;
                     } else if particle.x + particle.radius > 1.0 - 0.001 {
                         // near right wall (x = 1)
-                        new_particle.vx *= -0.7
+                        let force = 2.0 * particle.mass * new_particle.vx.abs() / frame_dt;
+                        mean_pressure = moving_average_constant * mean_pressure
+                            + (1.0 - moving_average_constant) * force;
+                        new_particle.vx *= -1.0
                     }
                     if particle.y - particle.radius < 0.001 {
                         // near top wall (y = 0)
@@ -357,9 +477,15 @@ fn main() {
                         //     "bouncing vertical, {}, {}, {}",
                         //     particle.x, particle.y, particle.radius
                         // );
+                        let force = 2.0 * particle.mass * new_particle.vy.abs() / frame_dt;
+                        mean_pressure = moving_average_constant * mean_pressure
+                            + (1.0 - moving_average_constant) * force;
                         new_particle.vy *= -1.0;
                     } else if particle.y + particle.radius > 1.0 - 0.001 {
                         // near bottom wall (y = 1)
+                        let force = 2.0 * particle.mass * new_particle.vy.abs() / frame_dt;
+                        mean_pressure = moving_average_constant * mean_pressure
+                            + (1.0 - moving_average_constant) * force;
                         new_particle.vy *= -1.0;
                     }
                 }
@@ -373,7 +499,7 @@ fn main() {
             // perform collision prediction for i and j with everything else.
             let indices = j.map_or(vec![i], |e| vec![i, e]);
             for idx in indices.iter() {
-                let mut new_events: Vec<(f32, f32, usize, Option<usize>)> = (0..num_particles)
+                let mut new_events: Vec<(f32, f32, usize, Option<usize>)> = (0..particles.len())
                     .into_par_iter()
                     .filter_map(|check_i| {
                         if check_i == i {
