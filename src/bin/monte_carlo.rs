e170202d8d81bfd30a03a38ab4105e90e9f046a4
Gillett Hernandez <gillett.hernandez@gmail.com>
2021-02-12 23:41:05 -0800

    get simple SMIS test working

--- src/bin/monte_carlo.rs
+++ src/bin/monte_carlo.rs
@@ -12,30 +12,33 @@ fn f(x: f32) -> f32 {
         + gaussian(x as f64, 2.4 * a2.recip(), 0.0, 0.3, 0.3)) as f32
 }
 
+const NUM_SAMPLES: usize = 8;
+
 fn smis_test() {
-    let mut ts: [f32; 4] = [0.0; 4];
-    let mut pdfs: [f32; 4] = [0.0; 4];
-    let mut fs: [f32; 4] = [0.0; 4];
+    let mut ts: [f32; NUM_SAMPLES] = [0.0; NUM_SAMPLES];
+    let mut pdfs: [f32; NUM_SAMPLES] = [0.0; NUM_SAMPLES];
+    let mut fs: [f32; NUM_SAMPLES] = [0.0; NUM_SAMPLES];
+    let mut xs: [f32; NUM_SAMPLES] = [0.0; NUM_SAMPLES];
 
     let c = std::f32::consts::PI / 2.0 - 0.2;
     let mut s = 0.0;
-    let n = 1000000;
+    let n = 100;
     for i in 0..n {
         // 4 technique + pdf samples per trial
-        for j in 0..4 {
+        for j in 0..NUM_SAMPLES {
             // get technique parameterization
             let t = 2.0 * random::<f32>() + 0.1;
             let m = t;
             // compute density transformation from using tangent function. (simply the derivative for this space, however in general a jacobian would be used for joint pdfs)
             ts[j] = t;
-            // sample according to gaussian(x, m, 0.0, 1.0/m)
+            // sample according to gaussian(x, 1.0/(m*sqrt(tau)), 0.0, m, m)
             let sigma = m.abs() as f64;
             let y_integral = random::<f64>();
             let x = sigma * std::f64::consts::SQRT_2 * erf::erf_inv(2.0 * y_integral - 1.0);
             // phi = 0.5 + 0.5 * erf(x/sqrt2rho)
             // 2.0 * phi - 1 = erf(x / sqrt2rho)
             // x = sqrt2rho * erfinv(2.0 * phi - 1)
-
+            xs[j] = x as f32;
             let y = gaussian(
                 x as f64,
                 (sigma * (std::f64::consts::TAU).sqrt()).recip(),
@@ -51,10 +54,25 @@ fn smis_test() {
         // computed fs, pdfs, and ts for samples 0 through 4.
         // now compute SMIS balance heuristic for 4 simultaneous samples.
 
-        // TODO: add SMIS balance heuristic
-        for j in 0..4 {
-            let i_j = fs[j] / pdfs[j];
-            s += i_j / 4.0 / n as f32;
+        let mut sum_ps: [f32; NUM_SAMPLES] = [0.0; NUM_SAMPLES];
+        for j in 0..NUM_SAMPLES {
+            let x = xs[j];
+            for t_j in 0..NUM_SAMPLES {
+                let sigma = ts[t_j] as f64;
+                let pj = gaussian(
+                    x as f64,
+                    (sigma * (std::f64::consts::TAU).sqrt()).recip(),
+                    0.0,
+                    sigma,
+                    sigma,
+                ) as f32;
+                sum_ps[j] += pj;
+            }
+        }
+        for j in 0..NUM_SAMPLES {
+            let w = pdfs[j] / sum_ps[j];
+            let i_j = w * fs[j] / pdfs[j];
+            s += i_j / n as f32;
             debug_assert!(!s.is_nan());
         }
     }
